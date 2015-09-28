915b25b6a97ac70333ea7014ab3cd736c6c27366
Yi Han <hanystudy@gmail.com>
2015-09-28 16:02:51 +0800

    send host and json file name at same time

--- json_fire/fire.rb
+++ json_fire/fire.rb
@@ -4,18 +4,19 @@ require 'json_fire'
 
 def report(msg)
   puts "error: #{msg} can not be empty, please check fixtures/host_setting.json"
-  puts "usage: fire.rb [path]"
+  puts "usage: fire.rb [url] [path]"
   exit 1
 end
 
 unless $*.empty?
-  path = $*[0].to_s
+  url = $*[0].to_s
+  path = $*[1].to_s
+  report 'url' if url.empty?
   report 'path' if path.empty?
   host_setting_file = File.new('./files/host_setting.json')
   host_setting = JSON.parse(host_setting_file.read, symbolize_names: true)
   host_setting_file.close
-  puts 'url' if host_setting[:url].to_s.empty?
-  response = JsonFire::JsonFire.send(host_setting[:url], path, host_setting)
+  response = JsonFire::JsonFire.send(url, path, host_setting)
   puts "#{response.code} #{response.msg}"
   puts response.body
   exit 0
